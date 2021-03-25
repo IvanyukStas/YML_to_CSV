d299a3a0058a0a4997d8a00f157285e42c778ac1
IvanyukStas <aswd25@yandex.ru>
2021-03-25 14:34:01 +0300

    add config

---
+++ config.py
@@ -0,0 +1,2 @@
+input_xml_file = 'test.xml'
+output_csv_file = 'test.csv'
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from bs4 import BeautifulSoup
 import csv
+import config
 '''
 КОВЕРТЕР ИЗ XML В CSV
 '''
 
 
 def record_to_csv(data):
-    with open('output.csv', 'a', encoding='cp1251', newline='') as file_w:
+    with open(config.output_csv_file, 'a', encoding='cp1251', newline='') as file_w:
         writer = csv.writer(file_w, delimiter=';', quoting=csv.QUOTE_MINIMAL)
         writer.writerows(data)
     print('RECORD COMLETE!')
 
 
 def extract_data_from_xml():
-    with open('test.xml', 'r', encoding='utf=8') as file:
+    with open(config.input_xml_file, 'r', encoding='utf=8') as file:
         xml_file = file.read()
 
     soup = BeautifulSoup(xml_file, 'lxml')
